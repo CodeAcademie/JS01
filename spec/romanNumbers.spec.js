d79c7c6c8b7eabe9bb551dca47b0dcc0b9ad241f
erwannFace <e.duclos@fondationface.org>
2016-05-06 14:40:25 +0200

    Initializing and Testing Travis

---
+++ spec/romanNumbers.spec.js
@@ -0,0 +1,10 @@
+var romanNumbers = require("../lib/romanNumbers")();
+
+describe('romanNumbers-js-kata', function(){
+
+  it('should have I when one passed', function() {
+    var result = romanNumbers.convertToRoman(1);
+    expect(result).toBe('I');
+  });
+
+});
